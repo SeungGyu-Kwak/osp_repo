2a3d535b3c968875d8a35fa40c929f267ef56322
SeungGyu-Kwak <tmdrb0912@naver.com>
2020-04-21 14:52:06 +0900

    folder

---
+++ py_lab2/my_pkg/BinToOther.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+def binToOther():
+    bin = input("input binary number : ")
+
+    D = int(bin,2) #2진수를 10진수로 바꿔준다.
+    O = format(D,'o') #10진수를 8진수로 바꿔준다.
+    H = format(D,'X') #10진수를 16진수로 바꿔준다.
+
+    print("OCT >> ",O)
+    print("DEC >> ",D)
+    print("HEX >> ", H)
+
+
