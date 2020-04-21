acf3cf9a0f131ecdfd67007fb04407ad2c5360ed
SeungGyu-Kwak <tmdrb0912@naver.com>
2020-04-22 00:15:33 +0900

    folder

--- py_lab2/myprog_pkg.py
+++ py_lab2/myprog_pkg.py
@@ -2,6 +2,21 @@
 
 import my_pkg
 
-my_pkg.binToOther()
-my_pkg.UnionIntersection()
+if __name__== '__main__':
+    
+    while True:
+        print("Select menu: 1)conversion 2)union/intersection 3)exit? ")
+        num = int(input(">>"))
+
+        if (num == 1):
+            my_pkg.binToOther()
+        elif (num == 2):
+            my_pkg.UnionIntersection()
+        elif (num == 3):
+            print("exit the program..")
+            break
+        else:
+            print("Wrong number. Input the number in (1~3)")
+
+
 
